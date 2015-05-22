f730019223e81cbaceaaeb50f1b97efc45130147
Manuel Casas <manuel.casasbarrado@gmail.com>
2015-05-22 08:33:40 +0200

    shared instance of scheduler

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 (function(require) {
   LazyLoader.load('lib/dom-scheduler.js', () => {
-    window.maestro = new DomScheduler();
+    var scheduler = new DomScheduler();
     LazyLoader.load(['gaia-dialog.js', 'gaia-dialog-alert.js',
       'gaia-dialog-confirm.js', 'gaia-dialog-prompt.js',
       'gaia-dialog-action.js', 'gaia-dialog-select.js', 'gaia-dialog-menu.js'],
@@ -8,9 +8,8 @@
         var gaiaDialogElements = document.querySelectorAll('gaia-dialog,' +
             'gaia-dialog-alert, gaia-dialog-confirm, gaia-dialog-prompt,' +
             'gaia-dialog-action, gaia-dialog-select, gaia-dialog-menu');
-        console.info(gaiaDialogElements);
         Array.prototype.forEach.call(gaiaDialogElements, elm => {
-            elm.scheduler = window.maestro;
+            elm.scheduler = scheduler;
         });
       });
   });
